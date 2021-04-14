08e429a171f078be0d17dab4773adaae7fa83567
Jake Robertson <jake@faltro.com>
2021-04-14 12:34:49 -0400

    add models to export

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-export {
-  FetchSeriesFunc,
-  ParseSeriesFunc,
-  FetchChaptersFunc,
-  ParseChaptersFunc,
-  FetchPageRequesterDataFunc,
-  ParsePageRequesterDataFunc,
-  GetPageUrlsFunc,
-  GetPageDataFunc,
-  FetchSearchFunc,
-  ParseSearchFunc,
-} from "./interface";
-
-export { ExtensionMetadata, PageRequesterData } from "./types";
+export * from "./models";
+export * from "./interface";
+export * from "./types";
